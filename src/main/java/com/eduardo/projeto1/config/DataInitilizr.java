ef68f7fc81752c10c358996bebd12a5068254291
eduhernandorena <edu_ard_o@hotmail.com>
2019-02-13 12:03:57 -0200

    Add files via upload
    projeto1

---
+++ src/main/java/com/eduardo/projeto1/config/DataInitilizr.java
@@ -0,0 +1,47 @@
+package com.eduardo.projeto1.config;
+
+import java.util.List;
+
+import com.eduardo.projeto1.entity.User;
+import com.eduardo.projeto1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitilizr implements ApplicationListener<ContextRefreshedEvent> {
+
+	@Autowired
+    UserRepository userRepository;
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent arg0) {
+
+		List<User> users = userRepository.findAll();
+
+		if (users.isEmpty()) {
+
+			createUser("Nataniel", "nataniel.paiva@gmail.com");
+			createUser("João","joao@gmail.com");
+			createUser("Maria", "maria@gmail.com");
+			
+		}
+		
+		User user = userRepository.findByEmailQualquerCoisa("nataniel.paiva@gmail.com");
+
+		System.out.println(user.getName());
+
+	}
+	
+	
+	public void createUser(String name, String email) {
+		
+		User user = new User(name, email);
+		
+		userRepository.save(user);
+		
+		
+	}
+
+}
---
+++ src/main/java/com/eduardo/projeto1/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.eduardo.projeto1.repository;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import com.eduardo.projeto1.entity.User;
+
+public interface UserRepository extends MongoRepository<User, Long> {
+
+	@Query("{ 'email' : ?0 }")
+	User findByEmailQualquerCoisa(String email);
+	
+	User findByNameIgnoreCaseLike(String name);
+	
+	
+}
